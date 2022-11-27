6adc1c1e0c28ef23cb4c4ab9fc60871a7637ad45
Egor Tenovoy <cofob@riseup.net>
2022-11-28 01:01:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,10 @@ apt-get install sudo -y
 echo "Installing direnv..."
 curl -sfL https://direnv.net/install.sh | bash
 
+# Install nix
+sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
+curl -L https://nixos.org/nix/install | sh
+
 # If pyproject.toml exists, install it with poetry
 if [ -f "pyproject.toml" ]; then
     echo "Installing poetry deps..."

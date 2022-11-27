6053d2df25227cc68389b6e77df763b75cfaeacd
Egor Tenovoy <cofob@riseup.net>
2022-11-28 00:55:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,13 @@
 rm $HOME/.bashrc
 cp .bashrc $HOME/
 
-# Update system
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install curl wget gcc python3 git -y
-
 # Install direnv
+echo "Installing direnv..."
 curl -sfL https://direnv.net/install.sh | bash
 
 # If pyproject.toml exists, install it with poetry
 if [ -f "pyproject.toml" ]; then
+    echo "Installing poetry..."
     # Install poetry
     curl -sSL https://install.python-poetry.org | python3 -
     sudo mv ~/.local/bin/poetry /bin/poetry
